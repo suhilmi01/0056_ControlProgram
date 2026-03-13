4aa63878921705e1f65e6b32570f3599831bc7f3
suhilmi01 <suhilmi01@mail.com>
2026-03-14 06:32:19 +0700

    program utama

--- program3.cpp
+++ program3.cpp
@@ -1,2 +1,17 @@
 #include <iostream>
 using namespace std;
+
+int main(){
+    int x;
+
+    srand(time(0));
+    x = rand() % 10;
+
+    if (x==5){
+        x = x+1;
+        cout <<"nilai x sudah dikoreksi menjadi : "<<x<<endl;
+    }
+
+    cout << "bilangan yang diberikan = "<<x;
+
+}
