568cbae924fa563e2a9efe2de5670e89c72e116e
suhilmi01 <suhilmi01@mail.com>
2026-03-14 06:27:18 +0700

    program utama

--- program2.cpp
+++ program2.cpp
@@ -4,6 +4,7 @@ using namespace std;
 float rata_rata(float a, float b){
     return (a+b)/2;
 }
+
 string status_lulus(float r){
     if (r >= 60)
     if (r >80){
@@ -14,4 +15,25 @@ string status_lulus(float r){
     }
     else
     return "gagal";
+}
+
+int main(){
+    float nilBI,nilMT,rerata;
+    string status;
+
+    cout <<"masukkan nilai bahasa inggris = ";
+    cin  >>nilBI;
+    cout << "masukkan nilai matematika = ";
+    cin >> nilMT;
+
+    //rerata = (nilBI,+nilMT)/2;
+    //if (rerata >= 60)
+    //    status = "lolos";
+    //else
+    //status = 'gagal";
+    rerata = rata_rata(nilBI,nilMT);
+    //status = status_lulus(rerata);
+
+    cout << "status kelulusan = "<<status_lulus(rerata)<<" ,dengan nilai rerata = "<<rerata<<endl;
+
 }
