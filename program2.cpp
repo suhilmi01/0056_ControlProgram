55027b577ce10a6e1148582556c855985374dfaf
suhilmi01 <suhilmi01@mail.com>
2026-03-14 06:21:34 +0700

    fungsi

--- program2.cpp
+++ program2.cpp
@@ -4,3 +4,14 @@ using namespace std;
 float rata_rata(float a, float b){
     return (a+b)/2;
 }
+string status_lulus(float r){
+    if (r >= 60)
+    if (r >80){
+        return "lulus cumlade";
+    }
+    else{
+        return "lulus baik";
+    }
+    else
+    return "gagal";
+}
