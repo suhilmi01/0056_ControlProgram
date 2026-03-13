7ae46c890f8772a7b00102734f8603d04aa0d7f2
suhilmi01 <suhilmi01@mail.com>
2026-03-14 05:47:09 +0700

    fungsi

---
+++ program1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+float rata_rata(float a, float b){
+    return (a+b)/2;
+}
+
+string status_lulus(float r){
+    if (r >= 60)
+        return "lolos";
+    else
+        return "gagal";
+}
+
